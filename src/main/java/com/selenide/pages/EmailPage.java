1e6a3e1800ba59b2fc648fa399c8a1a7c9dce2eb
Raman Patsiayuk <raman_patsiayuk@epam.com>
2018-01-03 12:24:37 +0300

    updated build gradle

--- src/main/java/com/selenide/pages/EmailPage.java
+++ src/main/java/com/selenide/pages/EmailPage.java
@@ -1,16 +1,9 @@
 package com.selenide.pages;
 
-import org.openqa.selenium.By;
-
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.title;
 
 public class EmailPage {
     private static final String EMAIL_PAGE_TITLE = "Входящие - Почта Mail.Ru";
-
-    public String getTitle() {
-        return title();
-    }
 
     public boolean isTitleCorrect() {
         return title().matches(EMAIL_PAGE_TITLE);
--- src/test/java/com/selenide/example/MailRuLoginTest.java
+++ src/test/java/com/selenide/example/MailRuLoginTest.java
@@ -2,6 +2,7 @@ package com.selenide.example;
 
 import com.selenide.pages.EmailPage;
 import com.selenide.pages.MailRuHomePage;
+import com.selenide.utils.PageTitlesConstants;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,6 +10,7 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
 import static com.selenide.utils.PageTitlesConstants.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -26,21 +28,20 @@ public class MailRuLoginTest {
     }
 
     @Test
-    @Ignore
+    //@Ignore
     public void loginToMailRuTest() {
         open("https://mail.ru/");
         $("input#mailbox\\:login").val(LOGIN);
         $("input#mailbox\\:password").val(PASSWORD);
         $("#mailbox\\:domain").selectOptionContainingText("@mail.ru");
         $("label#mailbox\\:submit").click();
-        $(By.xpath("//title")).shouldHave(text(EMAIL_PAGE_TITLE));
+        $(title()).shouldHave(text(EMAIL_PAGE_TITLE));
     }
 
     @Test
     public void loginToMailPoTest() {
         EmailPage emailPage = new MailRuHomePage().openHomePage().loginToMail();
-        //$(By.xpath("//title")).shouldHave(text(EMAIL_PAGE_TITLE));
-        //assertThat(emailPage.getTitle(), is(equalTo(EMAIL_PAGE_TITLE)));
-        assertThat("Title is correct", emailPage.isTitleCorrect());
+        assertThat(title(), is(equalTo(EMAIL_PAGE_TITLE)));
+        //assertThat("Title is not correct", emailPage.isTitleCorrect());
     }
 }
