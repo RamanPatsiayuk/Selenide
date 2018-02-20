7d1a53f3c5957d00bb99c8d40e8a8123afd734db
Raman Patsiayuk <raman_patsiayuk@epam.com>
2018-02-20 12:40:14 +0300

    updated BaseTest

--- src/test/java/com/selenide/example/BaseTest.java
+++ src/test/java/com/selenide/example/BaseTest.java
@@ -2,16 +2,20 @@ package com.selenide.example;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Screenshots;
+import com.codeborne.selenide.WebDriverRunner;
 import com.google.common.io.Files;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.WebDriver;
 import ru.yandex.qatools.allure.annotations.Attachment;
 
 import java.io.File;
 import java.io.IOException;
 
 public abstract class BaseTest {
+    WebDriver driver = WebDriverRunner.getWebDriver();
+
     @Before
     public void setUp() {
         Configuration.timeout = 6000;
@@ -24,6 +28,10 @@ public abstract class BaseTest {
         }catch (NullPointerException ex){
             ex.getMessage();
         }
+        /*if(driver != null){
+            driver.quit();
+        }*/
+
     }
 
     @Attachment(type = "image/png")
